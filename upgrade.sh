e9411879717cd312df6fc8f29fc87340ff6bb72e
Arun Kumar Rajamani <arunkumar.vediappan@gmail.com>
2019-04-11 23:56:37 +0530

    Share Function changes

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,4 @@
 [ -z "$1" ] && { echo "Usage: ./upgrade.sh versionNumber "; exit -1; }
-composer archive create --sourceType dir --sourceName . -a ./dist/healthcare-network@$1.bna
-composer network install -a ./dist/healthcare-network@$1.bna --card PeerAdmin@hlfv1
+composer archive create --sourceType dir --sourceName . -a ./healthcare-network@$1.bna
+composer network install -a ./healthcare-network@$1.bna --card PeerAdmin@hlfv1
 composer network upgrade --card PeerAdmin@hlfv1 --networkName healthcare-network --networkVersion $1
