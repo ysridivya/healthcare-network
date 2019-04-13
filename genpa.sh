cec33a711d3d62bf1c8a74f820aa727022b3111d
Sri Divya Yaddanapudi <sridivya6000@gmail.com>
2019-04-13 18:33:51 +0530

    Modified participant generation script

--- genpa.sh
+++ genpa.sh
@@ -1,3 +1,17 @@
+composer card delete  -c d1@healthcare-network
+composer card delete  -c d2@healthcare-network
+composer card delete  -c d3@healthcare-network
+composer card delete  -c p1@healthcare-network
+composer card delete  -c p2@healthcare-network
+composer card delete  -c p3@healthcare-network
+
+rm d1*
+rm d2*
+rm d3*
+rm p1*
+rm p2*
+rm p3*
+
 composer participant add -c  admin@healthcare-network -d '{"$class": "org1.healthcare.biznet.Doctor","DoctorId": "1","Name": "","Age": "","Address": "","phoneNumber": "","employedBy":"resource:org1.healthcare.biznet.Hospital#0322"}'
 ./genid.sh "org1.healthcare.biznet.Doctor#1" d1
 composer participant add -c  admin@healthcare-network -d '{"$class": "org1.healthcare.biznet.Doctor","DoctorId": "2","Name": "","Age": "","Address": "","phoneNumber": "","employedBy":"resource:org1.healthcare.biznet.Hospital#0322"}'
