7c77e16220afd0028e30f55c083273e97dd0c442
Sri Divya Yaddanapudi <sridivya6000@gmail.com>
2019-04-15 13:35:35 +0530

    Added Lab Report service

--- secure_healthcare_app/src/app/layout/bs-element/bs-element.component.ts
+++ secure_healthcare_app/src/app/layout/bs-element/bs-element.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { RestService} from '../../services/rest.service'
 @Component({
     selector: 'app-bs-element',
     templateUrl: './bs-element.component.html',
@@ -8,7 +9,23 @@ import { routerTransition } from '../../router.animations';
     animations: [routerTransition()]
 })
 export class BsElementComponent implements OnInit {
-    constructor() {}
-
-    ngOnInit() {}
+    labreports: Array<object>;
+    constructor(private route: ActivatedRoute,
+        private router: Router,
+        private restService: RestService) {
 }
+ngOnInit(){
+    this.restService.getPatientLabReport().subscribe(
+        data =>{
+            this.labreports = [];
+            for(let key in data)
+            {
+                var lab_id = data[key]['lab']
+                lab_id = lab_id.substr(lab_id.indexOf("#")+1);
+                data[key]['lab'] = lab_id
+                this.labreports.push(data[key]);
+                console.log(data[key]);
+            }
+        }
+    );
+ }}
--- secure_healthcare_app/src/app/services/rest.service.ts
+++ secure_healthcare_app/src/app/services/rest.service.ts
@@ -75,6 +75,10 @@ getAllHospitals(){
   return this.httpClient.get('http://localhost:3000/api/org1.healthcare.biznet.Hospital', {withCredentials: true})
 }
 
+getPatientLabReport(){
+  return this.httpClient.get('http://localhost:3000/api/org1.healthcare.biznet.LabReport', {withCredentials: true})
+}
+
 createAppointment(data) {
   console.log('createapoointment called by the user' + data.byPatient)
   const collector = {
